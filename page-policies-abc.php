19ba6a3ecee0583ef050e8fdb1366fa9722d4d41
mrcatalog <greg.mena@csun.edu>
2013-10-09 14:59:59 -0700

    Update page-policies-abc.php
    aplha order

--- page-policies-abc.php
+++ page-policies-abc.php
@@ -15,7 +15,7 @@ get_header();
 
 <h2>Policies: Alphabetical</h2>
 
-<?php $query_policies = new WP_Query(array('post_type' => dp_policy, 'orderby' => 'title', 'order' => 'DESC',  ));
+<?php $query_policies = new WP_Query(array('post_type' => dp_policy, 'orderby' => 'title', 'order' => 'ASC',  ));
 	if($query_policies->have_posts()) : while($query_policies->have_posts()) : $query_policies->the_post(); ?>
 		<h3><a href="<?php the_permalink();?>"/><?php the_title(); ?></a></h3>
 
